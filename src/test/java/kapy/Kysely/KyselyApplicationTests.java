291abaf44b43a4d7de7b9db49f2ca050e6277a27
pineappletea <47184314+pineappletea@users.noreply.github.com>
2020-04-02 09:41:49 +0300

    Initial commit

---
+++ src/test/java/kapy/Kysely/KyselyApplicationTests.java
@@ -0,0 +1,13 @@
+package kapy.Kysely;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KyselyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
