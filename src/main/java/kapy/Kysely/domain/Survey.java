c2cd2b1e8bcd5c1527529e0cd6634c4bdd4bb04e
pineappletea <47184314+pineappletea@users.noreply.github.com>
2020-04-07 13:59:18 +0300

    template methods, disabled security

--- src/main/java/kapy/Kysely/domain/Survey.java
+++ src/main/java/kapy/Kysely/domain/Survey.java
@@ -3,8 +3,8 @@ package kapy.Kysely.domain;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
-import org.springframework.data.annotation.Id;
 
 @Entity
 public class Survey {
--- src/main/java/web/SurveyController.java
+++ src/main/java/web/SurveyController.java
@@ -6,12 +6,15 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import kapy.Kysely.domain.Question;
 import kapy.Kysely.domain.QuestionRepository;
 import kapy.Kysely.domain.Survey;
 import kapy.Kysely.domain.SurveyRepository;
@@ -19,12 +22,42 @@ import kapy.Kysely.domain.SurveyRepository;
 @Controller
 @CrossOrigin
 public class SurveyController {
-	
+
 	@Autowired
 	SurveyRepository surveyRepository;
-	
+
 	@Autowired
 	QuestionRepository questionRepository;
+
+
+	// Create new survey, send to template. GET
+	@RequestMapping(value = "/addsurvey", method = RequestMethod.GET)
+	public String getNewSurvey(Model model) {
+		model.addAttribute("survey", new Survey());
+		return "addSurvey";
+	}
+
+	// Receive named survey from template, save it. Direct to adding questions. POST
+	@RequestMapping(value = "/addsurvey", method = RequestMethod.POST)
+	public String saveSurvey(@ModelAttribute Survey survey) {
+		surveyRepository.save(survey);
+		return "redirect:/addquestions";
+	}
+
+	// Create new question, send to template along with list of surveys. GET
+	@RequestMapping(value = "/addquestion", method = RequestMethod.GET)
+	public String getQuestion(Model model) {
+		model.addAttribute("question", new Question());
+		model.addAttribute("surveys", surveyRepository.findAll());
+		return "addQuestion";
+	}
+	// Receive question from template, save it. POST
+	// Direct to add another question.
+	@RequestMapping(value = "/addquestion", method = RequestMethod.POST)
+	public String saveQuestion(@ModelAttribute Question question) {
+		questionRepository.save(question);
+		return "redirect:/addquestions";
+	}
 
 	//Restful haetaan kysely id-numeron mukaan
 	@RequestMapping(value = "/surveys/{id}", method = RequestMethod.GET)
@@ -36,5 +69,5 @@ public class SurveyController {
 	public @ResponseBody List<Survey>surveyListRest(){
 		return (List<Survey>)surveyRepository.findAll();
 	}
-	
+
 }
