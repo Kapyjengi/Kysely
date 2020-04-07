8aef09b0aee5617174842295f5fc9b4c102d3e4c
Paivi Lampinen <bet983@myy.haaga-helia.fi>
2020-04-07 15:24:54 +0300

    KyselyApplication testidataa

--- src/main/java/kapy/Kysely/KyselyApplication.java
+++ src/main/java/kapy/Kysely/KyselyApplication.java
@@ -1,13 +1,44 @@
 package kapy.Kysely;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import kapy.Kysely.domain.Question;
+import kapy.Kysely.domain.QuestionRepository;
+import kapy.Kysely.domain.Survey;
+import kapy.Kysely.domain.SurveyRepository;
 
 @SpringBootApplication
 public class KyselyApplication {
+	private static final Logger log = LoggerFactory.getLogger(KyselyApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(KyselyApplication.class, args);
+	}
+	//testidatan luonti H2-testitietokantaan
+	@Bean
+	public CommandLineRunner surveyDemo(QuestionRepository questionRepository, SurveyRepository surveyRepository) {
+		return (args) -> {
+			log.info("save surveys and questions");
+			questionRepository.save(new Question("kysymys1"));
+			questionRepository.save(new Question("kysymys2"));
+			
+			surveyRepository.save(new Survey("kyselyn nimi 1", "kyselyn kuvaus 1"));
+			surveyRepository.save(new Survey("kyselyn nimi 2", "kyselyn kuvaus 2"));
+			
+			log.info("fetch all questions");
+			for (Question question : questionRepository.findAll()) {
+				log.info(question.toString());
+			}
+			log.info("fetch all surveys");
+			for (Survey survey : surveyRepository.findAll()) {
+				log.info(survey.toString());
+			}
+		};
 	}
 
 }
