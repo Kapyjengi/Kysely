4238b7d1ffd6d61ce087c87e4ea9301a4182d393
samikul <58463139+samikul@users.noreply.github.com>
2020-05-06 14:36:51 +0300

    Added testdata

--- src/main/java/kapy/Kysely/KyselyApplication.java
+++ src/main/java/kapy/Kysely/KyselyApplication.java
@@ -70,27 +70,96 @@ public class KyselyApplication {
 			questionRepository.save(question6);
 
 			log.info("test answers for survey1");
+			
+			// question 1
+			// Mitkä olivat tärkeimmät oppimistavoitteesi aloittaessasi opintojaksoa ja saavutitko ne?
 			Answer answer1 = new Answer("Oppimistavoitteeni kurssille oli oppia työskentelemään ryhmässä ja toteuttamaan toimivan vaatimukset täyttävän sovelluksen. Saavutimme kurssille annetut tavoitteet.",question1 );
 			answerRepository.save(answer1);
 
-			Answer answer2 = new Answer("Osaan paremmin hahmottaa, kuinka ohjelmistoprojektin työstäminen ryhmässä toimii.",question2 );
+			Answer answer2 = new Answer("Syventää rajapintojen käyttöä frontin ja backin kesken. Kyllä saavutin. ",question1 );
 			answerRepository.save(answer2);
-			Answer answer14 = new Answer("Asiat YKKÖNE, bee ja cee.",question2 );
-			answerRepository.save(answer14);
-			Answer answer15 = new Answer("Asiat KAKKONE, bee ja cee.",question2 );
-			answerRepository.save(answer15);
-
-			Answer answer3 = new Answer("Ryhmätyöskentely toimi hyvin tuttujen opiskelelukaveiden kanssa.",question3);
+			
+			Answer answer3 = new Answer("Syventää versiohallintaosaamista. Kyllä saavutin. ",question1 );
 			answerRepository.save(answer3);
-
-			Answer answer4 = new Answer("Olisin joissakin tilanteissa voinut aktiivisemmin tuoda mielipiteeni esiin.",question4);
+			
+			Answer answer4 = new Answer("Oppia toimimaan aktiivisemmin ryhmässä. Kyllä saavutin.. ",question1 );
 			answerRepository.save(answer4);
-
-			Answer answer5 = new Answer("Opintojaksolla opin uusia koodaamistaitoja sekä sain hieman kokemusta, kuinka ohjelmiston kehitys ryhmässä toimii.",question5);
+			
+			// question 2
+			// Mitkä ovat tärkeimmät opintojaksolla oppimasi taidot?
+			Answer answer5 = new Answer("Git-versiohallinnan käytön oppiminen.",question2 );
 			answerRepository.save(answer5);
 
-			Answer answer6 = new Answer("Ei tule mitään mieleen.",question6);
+			Answer answer6 = new Answer("Frontin ja backin liittäminen toisiinsa.",question2);
 			answerRepository.save(answer6);
+
+			Answer answer7 = new Answer("Opin tärkeitä yhteistyö- ja ryhmätyötaitoja.",question2);
+			answerRepository.save(answer7);
+
+			Answer answer8 = new Answer("Opintojaksolla opin uusia koodaamistaitoja sekä sain hieman kokemusta, kuinka ohjelmiston kehitys ryhmässä toimii.",question2);
+			answerRepository.save(answer8);
+
+			// question 3
+			// Kerro kokemuksesti ryhmätyöskentelystä.
+			Answer answer9 = new Answer("Ei tule mitään mieleen.",question3);
+			answerRepository.save(answer9);
+			
+			Answer answer10 = new Answer("Ryhmätyöskentely toimi hyvin tuttujen opiskelelukaveiden kanssa.",question3);
+			answerRepository.save(answer10);
+
+			Answer answer11 = new Answer("Olisin joissakin tilanteissa voinut aktiivisemmin tuoda mielipiteeni esiin.",question3);
+			answerRepository.save(answer11);
+
+			Answer answer12 = new Answer("Ihan ok, vaikka olisinkin toivonut enemmän aktiivisuutta muilta jäseniltä.",question3);
+			answerRepository.save(answer12);
+
+			Answer answer13 = new Answer("Yleisesti olen ollut tyytyväinen ryhmän jäsenten panokseen.",question3);
+			answerRepository.save(answer13);
+			
+			// question 4
+			// Mitä olisit voinut opintojaksolla tehdä toisin?
+			Answer answer14 = new Answer("Olisin voinut perehtyä enemmän backiin.",question4 );
+			answerRepository.save(answer14);
+
+			Answer answer15 = new Answer("Olisin voinut keskittyä enemmän fronttiin.",question4 );
+			answerRepository.save(answer15);
+			
+			Answer answer16 = new Answer("Olisin voinut olla oma-aloitteisempi.",question4 );
+			answerRepository.save(answer16);
+			
+			Answer answer17 = new Answer("Olisin voinut olla aktiivisempi.",question4 );
+			answerRepository.save(answer17);
+			
+			Answer answer18 = new Answer("Olisin voinut juoda vähemmän kahvia.",question4 );
+			answerRepository.save(answer18);
+
+			// question 5
+			// Mitä hyvää opintojaksossa oli?
+			Answer answer19 = new Answer("Opin scrumista ja sen menetelmistä enemmän sen vuoksi, että projekti toteutettiin scrumin avulla.",question5);
+			answerRepository.save(answer19);
+
+			Answer answer20 = new Answer("Konkreettisen kyselyapplikaation tekeminen oli mieluisaa.",question5);
+			answerRepository.save(answer20);
+
+			Answer answer21 = new Answer("Opintojaksolla opin uusia koodaamistaitoja sekä sain hieman kokemusta, kuinka ohjelmiston kehitys ryhmässä toimii.",question5);
+			answerRepository.save(answer21);
+
+			Answer answer22 = new Answer("Pääsimme toteuttamaan mielenkiintoisen projektin.",question5);
+			answerRepository.save(answer22);
+			
+			// question 6
+			// Miten kehittäisit opintojaksoa?
+			Answer answer23 = new Answer("Lisää demoja ja malliesimerkkejä.",question6);
+			answerRepository.save(answer23);
+
+			Answer answer24 = new Answer("Opettaja voisi kiertää Zoom-huoneita aktiivisesti ja käydä kyselemässä avuntarvetta.",question6);
+			answerRepository.save(answer24);
+
+			Answer answer25 = new Answer("Opettaja voisi antaa enemmän konreettisia vinkkejä.",question6);
+			answerRepository.save(answer25);
+
+			Answer answer26 = new Answer("Ei tule mitään mieleen.",question6);
+			answerRepository.save(answer26);
 
 			////////////////////
 			// TESTIKYSELY 2 //
@@ -118,25 +187,93 @@ public class KyselyApplication {
 
 			Question question13 = new Question("Miten kehittäisit opintojaksoa?", survey2, type1);
 			questionRepository.save(question13);
+			
 
 			log.info("test answers for survey1");
-			Answer answer8 = new Answer("Tärkein oppimistavoite kurssille oli oppia, kuinka tehdä toimiva backend nettisivulle. Kyllä, ymmärrän nyt mitä backendiltä halutaan ja mitä se tekee.",question8);
-			answerRepository.save(answer8);
+			
+			// question 8
+			// Mitkä olivat tärkeimmät oppimistavoitteesi aloittaessasi opintojaksoa ja saavutitko ne?
+			Answer answer27 = new Answer("Tärkein oppimistavoite kurssille oli oppia, kuinka tehdä toimiva backend nettisivulle. Kyllä, ymmärrän nyt mitä backendiltä halutaan ja mitä se tekee.",question8);
+			answerRepository.save(answer27);
 
-			Answer answer9 = new Answer("Opin tekemään backendin verkkosivulle.",question9 );
-			answerRepository.save(answer9);
+			Answer answer28 = new Answer("Tavoitteenani oli oppia tekemään backend verkkosivulle. Kyllä saavutin.",question8 );
+			answerRepository.save(answer28);
 
-			Answer answer10 = new Answer("Kurssilla ei toteutettu ryhmätyötä.",question10);
-			answerRepository.save(answer10);
+			Answer answer29 = new Answer("Halusin oppia yhden kokonaisen stackin perusteet. Kyllä saavutin.",question8);
+			answerRepository.save(answer29);
 
-			Answer answer11 = new Answer("Eipä oikeastaan mitään.",question11);
-			answerRepository.save(answer11);
+			Answer answer30 = new Answer("Halusin ymmärtää api-kutsuja. Kyllä.",question8);
+			answerRepository.save(answer30);
+			
+			// question 9
+			// Mitkä ovat tärkeimmät opintojaksolla oppimasi taidot?
+			Answer answer31 = new Answer("Opin  REST-metodeita.",question9);
+			answerRepository.save(answer31);
 
-			Answer answer12 = new Answer("Opintojaksossa oli hyvä, kun kurssilla toteutettiin omalle verkkosivuille yksilötyönä backend.",question12);
-			answerRepository.save(answer12);
+			Answer answer32 = new Answer("Opin paljon Spring Bootista ja osaan toteuttaa yksinkertaisen CRUD-sovelluksen, jossa on user ja admin käyttäjäluokat ja niille annetut käyttöoikeudet.",question9);
+			answerRepository.save(answer32);
+			
+			Answer answer33 = new Answer("Ymmärsin validoinnin periaatteen ja tärkeyden.",question9);
+			answerRepository.save(answer33);
 
-			Answer answer13 = new Answer("Ei tule mitään mieleen",question13);
-			answerRepository.save(answer13);
+			Answer answer34 = new Answer("Opin tekemään backendin verkkosivulle.",question9 );
+			answerRepository.save(answer34);
+
+			// question 10
+			// Kerro kokemuksesti ryhmätyöskentelystä.
+			Answer answer35 = new Answer("Kurssilla ei toteutettu ryhmätyötä.",question10);
+			answerRepository.save(answer35);
+
+			Answer answer36 = new Answer("Emme tehneet mitään ryhmässä.",question10);
+			answerRepository.save(answer36);
+
+			Answer answer37 = new Answer("Teimme omaehtoisesti ryhmässä töitä, mikä oli kivaa. Kurssiin ei kuulunut ryhmätyötä.",question10);
+			answerRepository.save(answer37);
+
+			Answer answer38 = new Answer("Ei ryhmätyötä. Ryhmätyö olisi auttanut oppimisen kannalta paljon.",question10);
+			answerRepository.save(answer38);
+			
+			// question 11
+			// Mitä olisit voinut opintojaksolla tehdä toisin?
+			Answer answer39 = new Answer("Olisin voinut aloittaa lopputyön tekemisen paljon aiemmin.",question11);
+			answerRepository.save(answer39);
+
+			Answer answer40 = new Answer("Olisin voinut paneutua paremmin opettajan demoihin ja materiaaleihin.",question11 );
+			answerRepository.save(answer40);
+
+			Answer answer41 = new Answer("Olisin voinut käyttää enemmän työtunteja kurssin ja varsinkin lopputyön eteen.",question11);
+			answerRepository.save(answer41);
+
+			Answer answer42 = new Answer("Olisi pitänyt käyttää monipuolisempia toimintoja lopputyössäni.",question11);
+			answerRepository.save(answer42);
+
+			// question 12
+			// Mitä hyvää opintojaksossa oli?
+			Answer answer43 = new Answer("Opettajan materiaalit olivat hyvät ja selkeät.",question12);
+			answerRepository.save(answer43);
+
+			Answer answer44 = new Answer("Ei tule mitään hyvää mieleen lol.",question12);
+			answerRepository.save(answer44);
+			
+			Answer answer45 = new Answer("Oli mielenkiintoista toteuttaa spring boot crud-projekti haluamastaan aiheesta.",question12);
+			answerRepository.save(answer45);
+
+			Answer answer46 = new Answer("Kurssilla oli mieltä avartava sisältö.",question12 );
+			answerRepository.save(answer46);
+
+			// question 13
+			// Miten kehittäisit opintojaksoa?
+			Answer answer47 = new Answer("Ei mitään kehitettävää. Kiitos.",question13);
+			answerRepository.save(answer47);
+
+			Answer answer48 = new Answer("Vapaaehtoisia syventäviä ja soveltavia tehtäviä, joita voi soveltaa esimerkiksi lopputyöhön.",question13);
+			answerRepository.save(answer48);
+
+			Answer answer49 = new Answer("Kurssin sisältö oli tosi hyvä lol ei kehitettävää.",question13);
+			answerRepository.save(answer49);
+
+			Answer answer50 = new Answer("Vähän lisää vinkkejä ja vippaskonsteja kiitos.",question13);
+			answerRepository.save(answer50);
 
 			log.info("fetch all surveys");
 			for (Survey survey : surveyRepository.findAll()) {
